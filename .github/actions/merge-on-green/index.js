f90daaf556c3dc864cae3baad21291e354c36f96
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-05-21 19:07:38 +0530

    Fix action

--- .github/actions/merge-on-green/index.js
+++ .github/actions/merge-on-green/index.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra')
 const eventPath = process.env.GITHUB_EVENT_PATH
 const eventName = process.env.GITHUB_EVENT_NAME
 
-const eventDataStr = await fse.readFile(eventPath, 'utf8')
+const eventDataStr = fse.readFileSync(eventPath, 'utf8')
 const eventData = JSON.parse(eventDataStr)
 
 console.log(eventData)
