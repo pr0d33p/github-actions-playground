dad129c6b59f69edac2259e3898767d8ecad5373
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-05-22 17:45:44 +0530

    Log event name

--- .github/actions/merge-on-green/index.js
+++ .github/actions/merge-on-green/index.js
@@ -7,3 +7,12 @@ const eventDataStr = fse.readFileSync(eventPath, 'utf8')
 const eventData = JSON.parse(eventDataStr)
 
 console.log(eventData)
+console.log(eventName)
+
+// if (
+//   eventData.action === 'labeled' ||
+//   eventData.action === 'submitted' ||
+//   eventData.action === 'completed'
+// ) {
+//   return
+// }
